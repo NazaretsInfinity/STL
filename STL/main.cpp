139b7e4ef16fb6d92c31508445a758118fba6cd1
Nazarets Ksenia <ksenianazarec995@gmail.com>
2024-08-26 03:18:49 -0700

    tried for list as well

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 #include<array>
 #include<vector>
+#include<list>
+#include<forward_list>
+
 using std::cin;
 using std::cout;
 using std::endl;
@@ -8,25 +11,53 @@ using std::endl;
 #define tab "\t"
 #define delimiter "\n---------------------------\n"
 
+
 template<typename T>void vector_properties(const std::vector<T>& vec);
 #define checking
 void main()
 {
+#ifdef checking1
 	std::vector<int> vec = { 3,5,8,13,21 };
 	vector_properties(vec);
 
-	std::vector<int>::iterator it = vec.begin();
 	int value, index;
 	cout << "Enter the index of new element: "; cin >> index;
 	cout << "Enter the value to add: "; cin >> value;
-	
-	vec.insert(it + index, value);
+
+	vec.insert(vec.begin()+index, value);
 	vector_properties(vec);
 
 	cout << "Enter the index where to delete: "; cin >> index;
 	vec.erase(vec.begin() + index);
 	vector_properties(vec);
+
+#endif 
+#ifdef checking2
+
+	std::list<int> mylist = { 3,5,8,13,21 };
+	for (int i : mylist)cout << i << tab; cout << endl;
+
+	int value, index;
+	std::list<int>::iterator it = mylist.begin();
+
+	cout << "Enter the index of new element: "; cin >> index;
+	cout << "Enter the value to add: "; cin >> value;
+	/*while (index)
+	{
+		++it; --index;
+	}*/
+	std::advance(it, index);
+	mylist.insert(it, value); it = mylist.begin();
+	for (int i : mylist)cout << i << tab; cout << endl;
+
+	cout << "Enter the index where to delete: "; cin >> index;
+	std::advance(it, index);
+	mylist.erase(it);
+	for (int i : mylist)cout << i << tab; cout << endl;
+#endif 
+
 }
+
 
 template<typename T>void vector_properties(const std::vector<T>& vec)
 {
