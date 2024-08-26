2018e723c79b472a95dd10ac09990979145b30ab
Nazarets Ksenia <ksenianazarec995@gmail.com>
2024-08-26 03:18:49 -0700

    tried for list as well

--- STL/SequenceContainers.cpp
+++ STL/SequenceContainers.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<array>
 #include<vector>
+#include<list>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -12,21 +13,50 @@ template<typename T>void vector_properties(const std::vector<T>& vec);
 #define checking
 void main()
 {
+#ifdef checking1
 	std::vector<int> vec = { 3,5,8,13,21 };
 	vector_properties(vec);
 
-	std::vector<int>::iterator it = vec.begin();
 	int value, index;
 	cout << "Enter the index of new element: "; cin >> index;
 	cout << "Enter the value to add: "; cin >> value;
-	
-	vec.insert(it + index, value);
+
+	vec.insert(vec.begin()+index, value);
 	vector_properties(vec);
 
 	cout << "Enter the index where to delete: "; cin >> index;
 	vec.erase(vec.begin() + index);
 	vector_properties(vec);
+
+#endif 
+#ifdef checking
+
+	std::list<int> mylist = { 3,5,8,13,21 };
+	for (int i : mylist)cout << i << tab; cout << endl;
+
+
+	int value, index;
+	std::list<int>::iterator it = mylist.begin();
+	cout << "Enter the index of new element: "; cin >> index;
+	cout << "Enter the value to add: "; cin >> value;
+	while (index)
+	{
+		++it; --index;
+	}
+	mylist.insert(it, value); it = mylist.begin();
+	for (int i : mylist)cout << i << tab; cout << endl;
+
+	cout << "Enter the index where to delete: "; cin >> index;
+	while (index)
+	{
+		++it; --index;
+	}
+	mylist.erase(it);
+	for (int i : mylist)cout << i << tab; cout << endl;
+#endif 
+
 }
+
 
 template<typename T>void vector_properties(const std::vector<T>& vec)
 {
