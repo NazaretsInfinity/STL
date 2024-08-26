46f0876cd79c880ae6c453315c2b9daa6b585314
Nazarets Ksenia <ksenianazarec995@gmail.com>
2024-08-26 02:42:13 -0700

    insert erase

--- STL/SequenceContainers.cpp
+++ STL/SequenceContainers.cpp
@@ -6,11 +6,33 @@ using std::cout;
 using std::endl;
 
 #define tab "\t"
+#define delimiter "\n---------------------------\n"
+
+template<typename T>void vector_properties(const std::vector<T>& vec);
+#define checking
 void main()
 {
 	std::vector<int> vec = { 3,5,8,13,21 };
-	for (int i : vec)
-	{
-		cout << i << tab;
-	}cout << endl;
+	vector_properties(vec);
+
+	std::vector<int>::iterator it = vec.begin();
+	int value, index;
+	cout << "Enter the index of new element: "; cin >> index;
+	cout << "Enter the value to add: "; cin >> value;
+	
+	vec.insert(it + index, value);
+	vector_properties(vec);
+
+	cout << "Enter the index where to delete: "; cin >> index;
+	vec.erase(vec.begin() + index);
+	vector_properties(vec);
+}
+
+template<typename T>void vector_properties(const std::vector<T>& vec)
+{
+	for (int i : vec)cout << i << tab; cout << endl;
+	cout << "Size:\t " << vec.size() << endl;
+	cout << "Capacity:" << vec.capacity() << endl;
+	cout << "MaxSize: " << vec.max_size() << endl;
+	cout << delimiter << endl;
 }
